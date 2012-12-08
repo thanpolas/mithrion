42aac2ac0a74ee47248f3c5544e05e047895cf30
Thanasis Polychronakis <thanpolas@gmail.com>
2012-12-08 19:48:52 +0200

    Made button blue

--- assets/js/mithrion-ck.js
+++ assets/js/mithrion-ck.js
@@ -1,4 +1,4 @@
 /**
    * Mock light sensor
    * @return {[type]} [description]
-   */function mock(){onLightChange({freq:mockFreq});mockFreq++}function onLightChange(e){log("onLightChange() called with val:"+e);var t=Math.floor(e/10);console.log("Playing chord:",t);soundApp.playChord(t)}var btn=document.getElementById("debug-note");btn.addEventListener("click",function(e){log("Clicked on debug btn");soundApp.playChord(9)});
+   */function mock(){onLightChange({freq:mockFreq});mockFreq++}function onLightChange(e){start(e*100)}var btn=document.getElementById("debug-note");btn.addEventListener("click",function(e){log("Clicked on debug btn");start(600)});
